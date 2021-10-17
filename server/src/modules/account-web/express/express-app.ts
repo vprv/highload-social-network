8377d792aa047c80db0d8cf36c3e86c1593f0949
Viacheslav Pereverzev <pereverzev.v.i@gmail.com>
2021-10-18 01:52:16 +0300

    Добавлен клиент

--- server/src/modules/account-web/express/express-app.ts
+++ server/src/modules/account-web/express/express-app.ts
@@ -2,19 +2,25 @@ const express = require("express");
 const authRoutes = require("./routes/auth.routes");
 const accountRoutes = require("./routes/account.routes");
 const app = express();
+const path = require('path');
 
 app.use(express.json({ extended: true }))
 
 app.use('/api/auth', authRoutes);
 app.use('/api/account', accountRoutes);
 
-app.get("/", (req: any, res: any) => {
-    res.json({ ping: "pong" })
-})
+
+if (process.env.NODE_ENV === 'PROD') {
+    app.use('/', express.static(path.join('client', 'build')))
+    app.get('*', (req: any, res: any) => {
+        res.sendFile(path.resolve('client', 'build', 'index.html'))
+    })
+}
+
 
 export async function startExpress(port: number) {
     try {
-        app.listen(port, () => console.log(`Server http://localhost:5000/`));
+        app.listen(port, () => console.log(`Server http://localhost:${port}/`));
     } catch (e: any) {
         console.log('Server Error', e.message)
         process.exit(1)
